27031fb75729ee81cccb7b35a16e5326c206cc9f
gbroques <groques360@gmail.com>
2015-10-19 14:53:31 -0500

    Updated category archive template

--- web/app/themes/sage/template-category-archive.php
+++ web/app/themes/sage/template-category-archive.php
@@ -9,8 +9,20 @@
 	<?php the_content(); ?>
 <?php endwhile; else: endif; ?>
 
-<?php query_posts('category_name='.get_the_title().'&post_status=publish,future');?>
+<?php query_posts('category_name='.get_permalink().'&post_status=publish,future');?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-		<p><?php the_content(); ?>
+		<div class="post-container">
+			<article>
+				<div class="post-header">
+				<a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a>
+				<p class="post-meta">
+					<?php get_template_part('templates/entry-meta'); ?>
+				</p>
+				</div>
+				<div class="post-content">
+					<?php the_excerpt(); ?>
+				</div>
+				<a href="<?php the_permalink(); ?>" class="read-more">Continue Reading...</a>
+			</article>
+		</div>
 	<?php endwhile; else: endif; ?>
--- web/app/themes/sage/templates/content.php
+++ web/app/themes/sage/templates/content.php
@@ -1,14 +1,14 @@
 <div class="post-container">
 	<article>
-	  <div class="post-header">
-	  <a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a>
-	  <p class="post-meta">
-	    <?php get_template_part('templates/entry-meta'); ?>
-	  </p>
-	  </div>
-	  <div class="post-content">
-	  	<?php the_excerpt(); ?>
-	  </div>
-	  <a href="#" class="read-more">Continue Reading...</a>
+		<div class="post-header">
+		<a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a>
+		<p class="post-meta">
+			<?php get_template_part('templates/entry-meta'); ?>
+		</p>
+		</div>
+		<div class="post-content">
+			<?php the_excerpt(); ?>
+		</div>
+		<a href="<?php the_permalink(); ?>" class="read-more">Continue Reading...</a>
 	</article>
 </div>
