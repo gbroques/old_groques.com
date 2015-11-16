e38fb35e3eb43e76be47e6307ef9bd2c8c061ee8
gbroques <groques360@gmail.com>
2015-11-16 13:05:04 -0600

    Added header to 'work' pages

---
+++ web/app/themes/sage/templates/work-header.php
@@ -0,0 +1,3 @@
+<h1 class="work-page-title">Branding &amp; Website Design</h1>
+<h2 class="work-sub-title align-left"><?php the_title() ?></h2>
+<h2 class="work-sub-title align-right">August 2015</h2>
