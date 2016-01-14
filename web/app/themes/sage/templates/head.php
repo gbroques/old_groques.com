20e85e54a2285d5baf75205c9e0e2d68e988daa8
gbroques <groques360@gmail.com>
2016-01-14 15:01:08 -0600

    Added favicon

--- web/app/themes/sage/templates/head.php
+++ web/app/themes/sage/templates/head.php
@@ -2,7 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.png">
   <link href='http://fonts.googleapis.com/css?family=Leckerli+One' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Nunito:400, 700' rel='stylesheet' type='text/css'>
